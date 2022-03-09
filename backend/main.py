5abfbf03141a01c02a504cb46340c5be1d0a42d4
dario <dario.mp@bluewin.ch>
2022-03-09 20:47:17 +0100

    Backend

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from crypt import methods
+from distutils.log import debug
+from flask import Flask, request
+    
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+
+@app.route('/feed', methods=['POST'])
+def feed():
+    blob = request.form
+    print(blob)
+    return 'Ok'
+
+@app.route('/give', methods=['POST'])
+def feed():
+    blob = request.form
+    print(blob)
+    return 'Ok'
+
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(debug=True)
