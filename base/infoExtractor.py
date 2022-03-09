3e5994a8be4a51f36917c8c76ce3105637b9af6c
Daniel Klauser <daniel.klauser@hslu.ch>
2022-03-09 21:00:53 +0100

    create base functions in info Extractor

--- base/infoExtractor.py
+++ base/infoExtractor.py
@@ -1,6 +1,7 @@
 import logging
+import json
 from base.configReader import Configuration
-
+from datetime import datetime
 from base.subscriber import Subscriber
 
 
@@ -13,3 +14,21 @@ class InfoExtractor:
         logging.basicConfig(
             level=logging.DEBUG, filename=config.config['name'] + '.log')
         self.subscriber = Subscriber(self.config, None)  # FIXME
+
+    def uploadBlobToS3(self, pathToData : str):
+        """Copies data to S3 bucket and returns storage adress"""
+        raise NotImplementedError
+
+    def insertDataIntoDatabase(self, infoExtractorName : str, result : str, blobPath : str):
+        """Sends the data to the restAPI"""
+        data = {
+            "proc_Name":infoExtractorName,
+            "timestamp": datetime.now().time() * 1000,
+            "result": result,
+            "link":blobPath
+        }
+        raise NotImplementedError
+
+
+
+
