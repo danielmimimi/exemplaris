ea73538499050c2232db8787dc4fffbf1b0e42d4
dario <dario.mp@bluewin.ch>
2022-03-09 21:51:01 +0100

    Fixed signatures

--- backend/db_feed.py
+++ backend/db_feed.py
@@ -6,12 +6,12 @@ class PostGreSQL:
     def __init__(self) -> None:
         self.connection = connect(dbname=environ['DB_NAME'], user=environ['DB_USER'], password=environ['DB_PASS'])
 
-    def insert(self, proc_name: str, date: int, result: str, link: str) -> int:
+    def insert(self, proc_name: str, date: int, result: str, link: str) -> None:
         with self.connection:
             with self.connection.cursor() as curs:
                 curs.execute(f"INSERT INTO {environ['TABLE_NAME']} ({proc_name}, {date}, {result}, {link});")
 
-    def fetch(self):
+    def fetch(self) -> None:
         with self.connection:
             with self.connection.cursor() as curs:
                 return [
